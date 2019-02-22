bd543e1392041bc157c548f99310f59d7b90f9b8
NatsukiPrayer <47858718+NatsukiPrayer@users.noreply.github.com>
2019-02-22 16:31:49 +0300

    Add files via upload

---
+++ Pr1_calc.cpp
@@ -0,0 +1,42 @@
+﻿
+
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b;
+	char operation;
+	cout << "Enter first number: ";
+	cin >> a;
+	cout << "Enter second number: ";
+	cin >> b;
+	cout << "Enter operation: ";
+	cin >> operation;
+
+	switch (operation)
+	{
+	case '-':
+		std::cout << "a - b = " << a - b << '\n';
+		break;
+	case '+':
+		std::cout << "a + b = " << a + b << '\n';
+		break;
+	case '*':
+		std::cout << "a * b = " << a * b << '\n';
+		break;
+	case '/':
+		while (b == 0) {
+			cout << "Devision by zero exception, re-enter b:";
+			cin >> b;
+		}
+		std::cout << "a / b = " << a / b << '\n';
+		break;
+	default:
+		std::cout << "Wrong operation\n";
+	}
+	return 0;
+}
+
+
---
+++ Pr1_name.cpp
@@ -0,0 +1,15 @@
+﻿
+#include "pch.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string name;
+    cout << "Enter your name:"; 
+	cin >> name;
+	cout << "Good evening, " << name << endl;
+	return 0;
+}
+
