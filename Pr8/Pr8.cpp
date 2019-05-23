d4a711b1ed1c3105e71671f8314c5eeba3714254
NatsukiPrayer <47858718+NatsukiPrayer@users.noreply.github.com>
2019-05-23 10:10:00 +0300

    Add files via upload

---
+++ Pr8/Pr8.cpp
@@ -0,0 +1,23 @@
+﻿// Pr8.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <string>
+#include <fstream> 
+using namespace std;
+
+
+int main()
+{
+	string temp;
+	char buff[15];
+	ifstream fin("NeverOpenThis1.txt");
+	while (!fin.eof()) {
+		fin >> buff;
+		cout << buff << endl;
+	}
+	fin.close();
+	return 0;
+}
+
